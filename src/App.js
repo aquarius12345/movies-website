722ad4bf9c4df3cdfbcda7ba90edc17c1151f5e1
Debora Seo <deborah_seo@hotmail.com>
2021-08-08 17:00:31 -0300

    changed text color in App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Home from './views/Home';
 function App() {
   return (
     <div className="App">
-    
+
+      <p>Carlos: Aqui no App tem que ficar o Header - pode deletar esta msg</p>
+
       <Switch>
         <Route exact path="/" component={Home}/>
       </Switch>
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,7 @@ import Banner from '../components/Banner';
 function Home() {
   return (
     <div className="home">
-      {/* <p>Carlos: Aqui fica o Header - pode deletar esta msg</p> */}
+      
       <Banner/>
     </div>
     
