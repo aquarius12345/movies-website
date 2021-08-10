e6e869fc84c9bc02fa3176eb3b9b91252d3f5540
Debora Seo <deborah_seo@hotmail.com>
2021-08-10 14:38:10 -0300

    created all pages inside home and components

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Recommend.css';
+import { Link } from 'react-router-dom';
 
 
 function Recommend(props) {
   const newData = props.data.slice(0, 9);
   return(
     <div className="rec">
-      <h2 id="recommend">{props.name}</h2>
+      <Link to={`/${props.link}`}><h2 id="recommend">{props.name}</h2></Link>
       <div className="list-img">
-        {newData.map((el)=> <div id="img-container"><img src={`https://image.tmdb.org/t/p/w300/${el.poster_path}`}/>
-        <span id="rec-vote">{"⭐"}{el.vote_average}</span>
-        <span id="rec-title">{el.original_title}</span>
+        {newData.map((el)=> <div id="img-container">
+        <Link to={`/movie-details/${el.id}`}>
+          <img src={`https://image.tmdb.org/t/p/w300/${el.poster_path}`}/>
+        </Link> 
+        <div className="span-container">
+          <span id="rec-vote">{"⭐"}{el.vote_average}</span>
+        <Link to={`/movie-details/${el.id}`}>
+          <span id="rec-title">{el.original_title}</span>
+        </Link>
+        </div>
         </div>)}
       </div>
       
---
+++ src/views/PopularPage.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React from 'react';
+import GenreCard from '../components/GenreCard';
+import './NowPage.css';
+
+
+
+class PopularPage extends React.Component {
+  
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    const firstrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=1";
+    const secondrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=11";
+    const thirdrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=3";
+    const fourthrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=4";
+    const fifthrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=5";
+    const sixthrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=6";
+    const seventhrequest = "https://api.themoviedb.org/3/movie/popular?api_key=8011d764f63852f7bf4dda520102d6c9&language=en-US&page=7";
+
+
+    axios.all([axios.get(firstrequest),
+           axios.get(secondrequest),
+           axios.get(thirdrequest),
+           axios.get(fourthrequest),
+           axios.get(fifthrequest),
+           axios.get(sixthrequest),
+           axios.get(seventhrequest)])
+     .then(axios.spread((firstResponse, secondResponse, thirdResponse, fourthResponse, fifthResponse, sixthResponse, seventhResponse) => {  
+        //console.log(firstResponse.data.results,secondResponse.data.results, thirdResponse.data.results);
+        this.setState ({
+          data: [...firstResponse.data.results, ...secondResponse.data.results, ...thirdResponse.data.results, 
+                ...fourthResponse.data.results, ...fifthResponse.data.results, ...sixthResponse.data.results, ...seventhResponse.data.results]
+        });
+     }));
+  
+  };
+
+
+  render() {
+    //console.log('this is data', this.state.data);
+    return (
+      <div className="now">
+        <h1 id="nowpage-title">Popular</h1>
+        {/* {this.state.data.map((el)=> <div><img src={`https://image.tmdb.org/t/p/w300/${el.poster_path}`}/></div>)} */}
+        <GenreCard data={this.state.data} genre="Science Fiction" genreNum={878}/>
+        <GenreCard data={this.state.data} genre="Comedy" genreNum={35}/>
+        <GenreCard data={this.state.data} genre="Drama" genreNum={18}/>
+        <GenreCard data={this.state.data} genre="Animation" genreNum={16}/>
+      </div>
+    );
+  };
+};
+
+export default PopularPage;
