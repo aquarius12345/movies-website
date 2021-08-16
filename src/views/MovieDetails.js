e80de7aef2478ddbc23128c7628b265bd8a0f1cb
kadu <kadu999@gmail.com>
2021-08-16 13:41:47 -0300

    styled-components-install

--- src/views/MovieDetails.js
+++ src/views/MovieDetails.js
@@ -1,13 +1,33 @@
+import Bluebird from 'bluebird'
+import { BackgroundColor, Color } from 'jest-matcher-utils/node_modules/chalk'
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+
+
 
 class MovieDetails extends Component {
+    
+    
     render() {
         return (
-            <div>
-                
-            </div>
+            <Container>
+                <h2>nome do filme</h2>
+            </Container>
+        
         )
     }
 }
 
-export default MovieDetails
+const Container = styled.div `
+   height: 500px;
+   background-color: black;
+
+  
+
+
+`;
+
+
+export default MovieDetails;
