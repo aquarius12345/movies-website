c818c4b4f653d31af3d90c33a7e841ef50642572
kadu <kadu999@gmail.com>
2021-08-20 00:02:51 -0300

    insertRating

--- src/views/MovieDetails.js
+++ src/views/MovieDetails.js
@@ -8,71 +8,64 @@ import './MoviesDetails.css';
 
 class MovieDetails extends Component {
     state = {
-        movies:[]
+        movies: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getData()
     }
 
-    getData(){
+    getData() {
         axios.get(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=34c0e6ee9353d4514d2f2c4122f49ae3&language=en-US
         `)
-         .then((result)=> {
-             console.log(result)
-            this.setState({
-                movies:result.data
+            .then((result) => {
+                console.log(result)
+                this.setState({
+                    movies: result.data
+                })
             })
-        })
-           
 
-      }
-        
-    
+
+    }
+
+
     render() {
         return (
             <div className='view'>
-                 <div className ="card-details">
-                 <h2>{this.state.movies.title}</h2>
-                 <p><span>release: {this.state.movies.release_date},
-                  minutos: {this.state.movies.runtime} 
+                <div className="card-details">
+                    <h2>{this.state.movies.title}</h2>
+                    <p><span>release: {this.state.movies.release_date},
+                        minutos: {this.state.movies.runtime}
                     </span></p>
-                    
-                    
+
+
                     <div className='card-poster'>
                         <div>
-                        <img src={`https://image.tmdb.org/t/p/w300/${this.state.movies.poster_path}`} alt="postermovie"/>
-                       {/*  <p>{this.state.movies.genres[2]}</p> */}
+                            <img src={`https://image.tmdb.org/t/p/w300/${this.state.movies.poster_path}`} alt="postermovie" />
+
                         </div>
-                    <div className ="trailer">
-                    <img src={`https://image.tmdb.org/t/p/original/${this.state.movies.backdrop_path}`} alt="postermovie"/>
-                    
-                    </div>
-
-                  
-
-                    </div>
-
-                        <div className='overview'>
-                            <h3>Overview:</h3>
-                            <p>{this.state.movies.overview}</p>
-
+                        <div className="trailer">
+                            <img src={`https://image.tmdb.org/t/p/original/${this.state.movies.backdrop_path}`} alt="postermovie" />
 
                         </div>
 
+                        <div className='rating'>
+                            <h3>RATING: {this.state.movies.vote_average}</h3> 
+                        </div>
 
-                    
-                   
+                    </div>
+
+                    <div className='overview'>
+
+                        <h3>Overview:</h3>
+                        <p>{this.state.movies.overview}</p>
 
 
+                    </div>
                 </div>
-           
-               
-
-
             </div>
-         
-        
+
+
         )
     }
 }
